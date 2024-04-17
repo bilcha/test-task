a56a9b062e9e7ddcb4f98def8179d6dedb101d80
bilcha <33802160+bilcha@users.noreply.github.com>
2024-04-17 21:20:38 +0000

    Deploying to gh-pages from @ bilcha/test-task@05f28ef764605d0fc87aafba890a878c0bffb3a4 🚀

--- static/js/664.b1bfea69.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_task=self.webpackChunktest_task||[]).push([[664],{664:function(t,e,s){s.r(e);s(791);var a=s(184);e.default=function(){return(0,a.jsx)("div",{children:"FavoritesPage"})}}}]);
-//# sourceMappingURL=664.b1bfea69.chunk.js.map
